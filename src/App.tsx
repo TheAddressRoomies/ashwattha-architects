b6b4fec90dd54512374c11bc88d8233172c930aa
Saurabh Pansare <saurabhpansare1122@gmail.com>
2024-05-21 21:34:05 +0530

    created connection to firebase db to fetch projects
    created connection to firebase db to fetch projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { database } from './config/firebaseConfig';
+import { ref, get } from 'firebase/database';
 
-function App() {
+interface Project {
+  id: number,
+  name: string,
+  description: string,
+  location: string
+  completionDate: string,
+  type: string,
+  photoUrls: Array<string>,
+  videoUrls: Array<string>
+
+}
+
+const App = () => {
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const projectsRef = ref(database, 'projects');
+        const snapshot = await get(projectsRef);
+
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          const projectsList: Project[] = Object.keys(data).map((key, index) => ({
+            id: index,
+            ...data[key]
+          }));
+          setProjects(projectsList);
+          console.log(projectsList)
+        } else {
+          console.log('No data available');
+        }
+      } catch (error) {
+        console.error('Error fetching projects:', error);
+      }
+    };
+
+    fetchProjects();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +51,15 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <ul>
+        {projects.map(project => (
+          <li key={project.id}>
+            <h2>{project.name}</h2>
+            <p>{project.description}</p>
+            <p>{project.location}</p>
+          </li>
+        ))}
+      </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
