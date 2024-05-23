c41fbaef46d0d0fe211a9f82a6ec3f57abdd4b43
Prathmesh Kashinath Jadhav <prajadhav1243@gmail.com>
2024-05-23 14:33:50 +0530

    Home page skeleton (#3)
    - add NavBar
    - add Carousel
    - add auto increment number layout
    - add footer

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ root.render(
   </React.StrictMode>
 );
 
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
