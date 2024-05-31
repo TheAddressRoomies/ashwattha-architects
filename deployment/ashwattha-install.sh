ae5d0e3cc98b1f1d97d5ba1298c254cc5163dd08
Omkar <47773862+OmkarDeshpande7@users.noreply.github.com>
2024-06-01 00:04:17 +0530

    install script enhancements (#24)
    * install script enhancements
    
    * typo

--- deployment/ashwattha-install.sh
+++ deployment/ashwattha-install.sh
@@ -3,8 +3,8 @@ set -x
 set -e
 # set -o pipefail
 
-git clone https://github.com/TheAddressRoomies/ashwattha-architects.git
-
+git clone https://github.com/TheAddressRoomies/ashwattha-architects.git | true
+cd ashwattha-architects/deployment
 # Check if the number of arguments is not equal to 4
 if [ "$#" -ne 4 ]; then
     echo "Usage: $0 <domain_name> <mysql_username> <mysql_password> <mysql_db_name>"
@@ -31,6 +31,10 @@ sudo apt-get update
 
 # Install Java (OpenJDK)
 sudo apt-get install -y openjdk-17-jdk
+sudo apt-get install -y maven
+
+# Install npm
+sudo apt install -y npm
 
 # Install Node.js and npm for React
 #curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
@@ -49,14 +53,11 @@ sudo systemctl start mysql
 sudo systemctl enable mysql
 
 # Install nginx
-sudo apt install nginx
+sudo apt install -y nginx
 
 # Print Java, Node.js, npm, and MySQL versions
 echo "Java version:"
 java -version
-
-echo "Node.js version:"
-node -v
 
 echo "npm version:"
 npm -v
@@ -76,30 +77,46 @@ cp ashwattha-react-app.service /lib/systemd/system
 echo "creating nginx confiuration"
 cat <<EOF > /etc/nginx/sites-available/$1 
 server {
+
     listen 80;
-    server_name $1 www.$1
-    location / {
-        proxy_pass http://127.0.0.1:your-port;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host $host;
-        proxy_cache_bypass $http_upgrade;
+    listen [::]:80;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+            server_name $1 www.$1; 
+                location / {
+
+                    proxy_pass http://localhost:3000;
+
+                    proxy_http_version 1.1;
+
+                    proxy_set_header Upgrade \$http_upgrade;
+
+                    proxy_set_header Connection 'upgrade';
+
+                    proxy_set_header Host \$host;
+
+                    proxy_cache_bypass \$http_upgrade;
+
+                }
     }
-}
 EOF
 
 echo "creating nginx softlink"
 ln -sf /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/$1
 
-echo "configuring mysql user and database"
-mysql -e "CREATE USER IF NOT EXISTS '$2'@'localhost' IDENTIFIED BY '$4';"
-mysql -e "CREATE DATABASE IF NOT EXISTS $3;"
-mysql -e "GRANT ALL PRIVILEGES ON $3.* TO '$2'@'localhost';"
+sudo systemctl restart nginx
 
-cd ashwattha-architects/backend
+echo "configuring mysql user and database"
+mysql -e "CREATE USER IF NOT EXISTS '$2'@'localhost' IDENTIFIED BY '$3';"
+mysql -e "CREATE DATABASE IF NOT EXISTS $4;"
+mysql -e "GRANT ALL PRIVILEGES ON $4.* TO '$2'@'localhost';"
+
+cd ../backend
 mvn clean install
 mv target/studioashwattha-backend-0.0.1-SNAPSHOT.jar /etc/developers
+
+cd ../frontend
+npm install
 
 systemctl daemon-reload
 systemctl start ashwattha-react-app
@@ -107,6 +124,7 @@ systemctl enable ashwattha-react-app
 systemctl start ashwattha-springboot-app
 systemctl enable ashwattha-springboot-app
 
+cd ../deployment
 echo "Copying mysql backup cronjob"
 cp -f backup_mysql.sh /etc/cron.d/backup_mysql.sh
 cp -f mysql_backup_cronjob /etc/cron.d/mysql_backup_cronjob
